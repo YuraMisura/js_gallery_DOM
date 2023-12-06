ec2f4a2692b13f0ee41c8fdd22dfad9f7650799b
Юрий Мисюра <apriory13@meta.ua>
2023-12-06 12:50:28 +0200

    make build

---
+++ index.7af9dd5e.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("largeImg");function t(t){t.preventDefault();var r=t.currentTarget.querySelector("a").getAttribute("href");e.setAttribute("src",r)}document.querySelectorAll(".list-item").forEach(function(e){return e.addEventListener("click",t)})});//# sourceMappingURL=index.7af9dd5e.js.map
+
+//# sourceMappingURL=index.7af9dd5e.js.map
